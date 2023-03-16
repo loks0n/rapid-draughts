22e083fe8516614db012877cc24ff14224210612
loks0n <22452787+loks0n@users.noreply.github.com>
2023-03-16 11:25:13 +0000

    test: add some more tests at game level

---
+++ src/english/game.test.ts
@@ -0,0 +1,36 @@
+import { describe, beforeEach, test, assert } from 'vitest';
+import { DraughtsPlayer } from '../core/engine';
+
+import { DraughtsGame1D } from '../core/game';
+import { EnglishDraughtsEngineStore } from './engine';
+import { EnglishDraughts } from './game';
+
+describe('start position', () => {
+  let draughts: DraughtsGame1D<number, EnglishDraughtsEngineStore>;
+
+  beforeEach(() => {
+    draughts = EnglishDraughts.setup();
+  });
+
+  test('corect opening player', () => {
+    assert.equal(draughts.player, DraughtsPlayer.DARK);
+  });
+
+  test('correct opening moves', () => {
+    assert.sameDeepMembers(draughts.moves, [
+      { origin: 8, destination: 12, captures: [] },
+      { origin: 8, destination: 13, captures: [] },
+      { origin: 11, destination: 15, captures: [] },
+      { origin: 10, destination: 14, captures: [] },
+      { origin: 10, destination: 15, captures: [] },
+      { origin: 9, destination: 13, captures: [] },
+      { origin: 9, destination: 14, captures: [] },
+    ]);
+  });
+
+  test('can make opening move', () => {
+    assert.doesNotThrow(() => {
+      draughts.move({ origin: 8, destination: 12, captures: [] });
+    });
+  });
+});
